3cdaf765b591aec15e123a02a8e14a23f7cc6892
Nicolas Roldan <nicolasroldan1994@gmail.com>
2022-03-29 13:25:38 -0300

    challenge sameFrequency

--- section06-challenges/03-sameFrequency.js
+++ section06-challenges/03-sameFrequency.js
@@ -8,12 +8,14 @@
 // sameFrequency(3589578, 5879385); // true
 // sameFrequency(22, 222); // false
 
+// My solution
+
 function sameFrequency(num1, num2) {
   const valuesNum1 = num1.toString();
   const valuesNum2 = num2.toString();
   if (valuesNum1.length !== valuesNum2.length) {
-		return false;
-	}
+    return false;
+  }
   let frequencyCounter1 = {};
   let frequencyCounter2 = {};
 
@@ -25,8 +27,8 @@ function sameFrequency(num1, num2) {
     frequencyCounter2[digit] ? frequencyCounter2[digit]++ : frequencyCounter2[digit] = 1;
   }
 
-  for(let key in valuesNum1) {
-    if (valuesNum1[key] !== valuesNum2[key]) {
+  for (let key in frequencyCounter1) {
+    if (frequencyCounter1[key] !== frequencyCounter2[key]) {
       return false;
     }
   }
@@ -37,3 +39,27 @@ console.log(sameFrequency(182, 281)); // true
 console.log(sameFrequency(34, 14)); // false
 console.log(sameFrequency(3589578, 5879385)); // true
 console.log(sameFrequency(22, 222)); // false
+
+// Other possible solution
+// function sameFrequency(num1, num2) {
+//   let strNum1 = num1.toString();
+//   let strNum2 = num2.toString();
+//   if (strNum1.length !== strNum2.length) return false;
+
+//   let countNum1 = {};
+//   let countNum2 = {};
+
+//   for (let i = 0; i < strNum1.length; i++) {
+//     countNum1[strNum1[i]] = (countNum1[strNum1[i]] || 0) + 1;
+//   }
+
+//   for (let j = 0; j < strNum1.length; j++) {
+//     countNum2[strNum2[j]] = (countNum2[strNum2[j]] || 0) + 1;
+//   }
+
+//   for (let key in countNum1) {
+//     if (countNum1[key] !== countNum2[key]) return false;
+//   }
+
+//   return true;
+// }
